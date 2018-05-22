f26473fdb90cfacfb4a261d2669c5cd8cbae890f
JakubKluk <klukjakub@gmail.com>
2018-05-22 16:59:27 +0200

    Added find_class funktion

--- Program ciapongi/src/ciapong.cpp
+++ Program ciapongi/src/ciapong.cpp
@@ -36,8 +36,46 @@
 
 }
 
+int find_class(const std::string& name)
+{
+    std::fstream file;                                  //nazwa służąca do odwoływania się do pliku
+    file.open("ciapongi.txt", std::ios::in);            //otwieramy plik
+        if(file.good()==false)
+    {
+        std::cout<<"Nie ma pliku"<<std::endl;
+                 exit(0);
+    }                                                   //sprawdzamy czy otwarcie przebiegło pomyślnie, jeśli nie, kończymy program
+    std::string line;
+    while(getline(file, line))                          //przeszukujemy każdą linijkę pliku aż do znalezienia naszej nazwy lub końca pliku
+    {
+        if(line==name)
+        {
+            getline(file, line);                        //Każdy getline przesuwa pobieranie tekstu o jedną linijkę w dół. Po znalezieniu nazwy jesteśmy na linijce "CLASS", więc musimy przesunąć się o linijkę niżej
+            grtline(file, line);
+            int i;
+            i = std::stoi(line);                        //Konwersja std::string na int przy użyciu funkcji biblioteki standardowej
+            file.close();                               //zamknięcie pliku
+            return i;
+        }
+    }
+    file.close();
+    std::cout<<"Nie ma takiego pociągu"<<std::endl;     //jeśli nie znajdziemy nazwy naszego pociągu, zamykamy plik i kończymy program
+    exit(0);
+}
 
 
+Route find_tour(std:: string start, std::string  end)//przekazujemy nazwy miast(stacji) z której chcemy jechac i gdzie dojechac
+{
+  auto number_start = find_station(start).getNumber();
+  auto number_end = find_station(end).getNumber();
 
+  std::fstream plik_s;
+  plik_s.open("pociagi.txt", std::ios::in);
+  if(plik_s.good()==false)
+  {
+      std::cout<<"Nie ma pliku"<<std::endl;
+      exit(0);
+  }
+//ciag dalszy nastapi
 
-
+}
